3980bf4c57e2e2d2b0c4c784c07d06864174185c
John R. Reigart III <routtatout@gmail.com>
2012-07-17 17:27:41 -0700

    stoping point on sign-up

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,7 +5,16 @@ describe "UserPages" do
 
   describe "signup page" do
     let( :section_name ) { "Sign Up" }
-    before  { visit signup_path }
+    before { visit signup_path }
+
+    it { should have_selector 'h1', text: section_name }
+    it { should have_selector 'title', text: full_title( section_name ) }
+  end
+
+  describe "profile page" do
+    let( :user ) { FactoryGirl.create( :user ) }
+    let( :section_name ) {  user.name }
+    before { visit user_path( user ) }
 
     it { should have_selector 'h1', text: section_name }
     it { should have_selector 'title', text: full_title( section_name ) }
