70b58c68c6487f0c7161e716a5abe50ca39b6af5
John R. Reigart III <routtatout@gmail.com>
2012-07-20 16:14:00 -0700

    SSL in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,10 @@ SampleApp::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Force all access to the app over SSL, use Strict-Transport-Security, 
+  # and use secure cookies.
+  config.force_ssl = true
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
@@ -26,9 +30,6 @@ SampleApp::Application.configure do
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
